21fa277e28a2a9fee35310f94352dfdb65da35a1
Jberna-bsale <jberna@bsale.com.pe>
2023-09-19 12:19:29 -0500

    Section Discover culminado

--- src/components/Section/SectionDifferent.svelte
+++ src/components/Section/SectionDifferent.svelte
@@ -1,3 +1,79 @@
-<section>
-  <h1>Diego</h1>
+<!-- <script>
+  let cards = [
+    {
+      src: "/src/img/badge 1.png",
+      alt: "badge",
+      title: "Supreme Beans",
+      text: "Beans that provides great taste"
+    },{
+      src: "/src/img/coffee-beans 1.png",
+      alt: "coffe beans",
+      title: "High Quality",
+      text: "We provide the highest quality"
+    },{
+      src: "/src/img/coffee-cup 1.png",
+      alt: "coffee cup",
+      title: "SExtraordinary",
+      text: "Coffee like you have never tasted"
+    },{
+      src: "/src/img/best-price 1.png",
+      alt: "best price",
+      title: "Affordable Price",
+      text: "Our Coffee prices are easy to afford"
+    },
+  ]
+</script>
+
+<section class="section">
+  <div class="titles">
+    <h1>Why are we different?</h1>
+    <p>We don’t just make your coffee, we make your day!</p>
+  </div>
+  <div class="cards">
+    {#each cards as card}
+      <div class="card">
+        <img src={card.src} alt={card.alt}/>
+        <h2>{card.title}</h2>
+        <p>{card.text}</p>
+      </div>
+    {/each}
+  </div>
+  <div class="footer">
+    <p>
+      Great ideas start with great coffee, Lets help you achieve that
+      Get started today.
+    </p>
+    <button>Join Us</button>
+  </div>
 </section>
+
+<style>
+  .section {
+    background-color: grey;
+    margin: 93px;
+    /* display: flex; */
+    /* flex-direction: column; */
+    /* align-items: space-between; space-around; space-evenly*/
+    /* justify-content: space-around; */
+  }
+
+  .cards {
+    background-color: blue;
+    display: flex;
+    flex-direction: row;    
+  }
+
+  .cards img {
+    height: 88px;
+  }
+
+  .card h2 {
+    font-family: Playfair Display;
+    font-size: 28px;
+    font-weight: 700;
+    line-height: 37px;
+    letter-spacing: 0em;
+    text-align: left;
+
+  }
+</style> -->
--- src/components/Section/SectionDiscover.svelte
+++ src/components/Section/SectionDiscover.svelte
@@ -1,3 +1,69 @@
-<section>
-  <h1>Jonathan</h1>
+<script>
+  // let src = "../../img/tacita_coffee.png"
+  let src = "/src/img/tacita_coffee.png"
+</script>
+
+<section class="section">
+  <div class="description">
+    <h1>Discover the best coffee</h1>
+    <p>
+      Bean Scene is a coffee shop that provides you with quality coffee that helps boost your productivity and helps build your mood. Having a cup of coffee is good, but having a cup of real coffee is greater. There is no doubt that you will enjoy this coffee more than others you have ever tasted.     
+    </p>
+    <button class="button">
+      Learn More
+    </button>
+  </div>
+  <img {src} alt="coffe" />
 </section>
+
+<style>
+  .section {
+    display: flex;
+    flex-direction: row;
+    margin: 93px;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+
+  }
+
+  h1 {
+    font-weight: 700;
+    font-size: 54px;
+    line-height: 72px;
+  }
+
+  p {
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 34px;
+    letter-spacing: 0em;
+    text-align: left;
+    width: 585px;
+    font-family: 'Playfair Display', bold;
+
+  }
+
+  img {
+    width: 500px;
+    height: 484px;
+  }
+
+  .description {
+    /* background-color: blue; */
+    display: flex;
+    flex-direction: column;
+    row-gap: 29px;    
+  }
+
+  .button {
+    border-radius: 24px;
+    border: none;
+    height: 48px;
+    width: 134px;
+    background-color: #F9C06A;
+    color: #1E1E1E;
+    font-size: 16px;
+    font-weight: 700;
+  }
+</style>
